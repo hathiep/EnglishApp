dc29b97489b7de9241159aecc61c2f7c65bcaf3c
B20DCCN620_Trần Thanh Tuấn <@tranthanhtuan01092002>
2024-03-15 09:34:08 +0700

    update exercise basic and voice

---
+++ app/src/main/java/com/example/applayout/core/exercise/ExerciseResultActivity.java
@@ -0,0 +1,55 @@
+package com.example.applayout.core.exercise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.applayout.R;
+
+public class ExerciseResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.exercise_result_activity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //back next home
+        ImageView icNext = findViewById(R.id.ic_next);
+        icNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ExerciseMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //---------------------
+
+        setDataResult();
+
+    }
+
+    private void setDataResult() {
+        TextView tvResult = findViewById(R.id.tv_result);
+        Intent intent = getIntent();
+        int result = intent.getIntExtra("result", 0);
+        String dataResult = "Chúc mừng bạn đã hoàn thành " + result*10 + "%";
+        tvResult.setText(dataResult);
+    }
+
+
+}
