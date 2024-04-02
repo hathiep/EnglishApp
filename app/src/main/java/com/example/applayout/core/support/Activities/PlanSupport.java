0ae14b31eda5900720c280b861525e78b4bab3d0
B20DCCN728 <vietjohnny810@gmail.com>
2024-04-02 16:56:51 +0700

    Update firebase, add taskbar buttons

--- app/src/main/java/com/example/applayout/core/support/Activities/PlanSupport.java
+++ app/src/main/java/com/example/applayout/core/support/Activities/PlanSupport.java
@@ -2,14 +2,20 @@ package com.example.applayout.core.support.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.applayout.R;
+import com.example.applayout.core.Profile;
+import com.example.applayout.core.exam.ExamMain;
+import com.example.applayout.core.exercise.ExerciseMain;
+import com.example.applayout.core.learn.LearnMain;
 import com.example.applayout.core.support.Adapters.PlanAdapter;
 import com.example.applayout.core.support.Domains.PlanDomain;
 import com.example.applayout.core.support.SupportMain;
@@ -19,6 +25,11 @@ import java.util.ArrayList;
 public class PlanSupport extends AppCompatActivity {
     private RecyclerView.Adapter adapterPlan;
     private RecyclerView recyclerViewPlan;
+
+    private ImageView back_button;
+    private ViewFlipper viewFlipper;
+    private Button nextFlipperButton;
+    private Button backFlipperButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,6 +116,54 @@ public class PlanSupport extends AppCompatActivity {
 
         adapterPlan = new PlanAdapter(items);
         recyclerViewPlan.setAdapter(adapterPlan);
+
+        ImageView imV_home = findViewById(R.id.imV_home);
+        ImageView imV_learn = findViewById(R.id.imV_learn);
+        ImageView imV_exercise = findViewById(R.id.imV_exercise);
+        ImageView imV_support = findViewById(R.id.imV_support);
+        ImageView imV_profile = findViewById(R.id.imV_profile);
+        ImageView imv_exam = findViewById(R.id.imV_exam);
+
+        imV_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), com.example.applayout.core.MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imV_learn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LearnMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imV_exercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ExerciseMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imv_exam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ExamMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imV_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
