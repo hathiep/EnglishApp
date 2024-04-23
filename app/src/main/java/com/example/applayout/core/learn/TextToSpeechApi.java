d88b8961f150d6b93535ddfcf0722c1dcc395b42
trongpd2407 <trongpd2407@gmail.com>
2024-04-24 00:08:45 +0700

    Update chức năng học từ mới

--- app/src/main/java/com/example/applayout/core/learn/TextToSpeechApi.java
+++ app/src/main/java/com/example/applayout/core/learn/TextToSpeechApi.java
@@ -9,9 +9,9 @@ import retrofit2.http.POST;
 
 public interface TextToSpeechApi {
     @Headers({
-            "content-type': 'application/json",
-            "X-RapidAPI-Key': '92fa9c0d25mshbe6f0d82c25b687p14f130jsn62a7e2fa8ae6",
-            "X-RapidAPI-Host': 'ai-powered-text-to-speech1.p.rapidapi.com"
+            "content-type: application/json",
+            "X-RapidAPI-Key: 8df0ce3dcdmsh9bb16beb49b1d21p1cc793jsn1c4e07d814dd",
+            "X-RapidAPI-Host: ai-powered-text-to-speech1.p.rapidapi.com"
     })
     @POST("synthesize-speech")
     Call<TextToSpeechResponse> synthesizeSpeech(@Body TextToSpeechRequestBody requestBody);
