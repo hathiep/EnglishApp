7983d87abd4d6c6bd62c50ab48d3089e403ef90a
trongpd2407 <trongpd2407@gmail.com>
2024-05-07 02:29:35 +0700

    Half final learn , update apikey

--- app/src/main/java/com/example/applayout/core/learn/TextToSpeechApi.java
+++ app/src/main/java/com/example/applayout/core/learn/TextToSpeechApi.java
@@ -10,7 +10,7 @@ import retrofit2.http.POST;
 public interface TextToSpeechApi {
     @Headers({
             "content-type: application/json",
-            "X-RapidAPI-Key: 8df0ce3dcdmsh9bb16beb49b1d21p1cc793jsn1c4e07d814dd",
+            "X-RapidAPI-Key: fb162dd0f3msh5846e815fcd2ad6p129fc6jsn1dad6c0b4fa1",
             "X-RapidAPI-Host: ai-powered-text-to-speech1.p.rapidapi.com"
     })
     @POST("synthesize-speech")
